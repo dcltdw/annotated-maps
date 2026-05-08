d7eef98f813d51a0aec471dea3f7bd3963ec2656
David Leung <dcltdw@mit.edu>
2026-05-08 16:36:29 -0400

    Added is_binary() check to ignore those files

--- scripts/count_lines.py
+++ scripts/count_lines.py
@@ -11,6 +11,18 @@ IGNORE_FILES = {
 }
 
 
+# Source - https://stackoverflow.com/a/51495076
+# Posted by Serhii, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-05-08, License - CC BY-SA 4.0
+def is_binary(file_name):
+    try:
+        with open(file_name, 'tr') as check_file:  # try open file in text mode
+            check_file.read()
+            return False
+    except:  # if fail then file is non-text (binary)
+        return True
+
+
 def examine_directory(path):
     results = subprocess.run(
         ['git', 'ls-files'],
@@ -24,6 +36,8 @@ def examine_directory(path):
             continue
         filename = path / fname
         if filename.name in IGNORE_FILES:
+            continue
+        if is_binary(str(filename)):
             continue
         # errors='replace' so a future binary commit (favicon, screenshot)
         # doesn't crash the count — those rows still get counted as 1+
